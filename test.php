0dedc3b5c879f81c5738d1c04d15e2ed159b34a5
musickappa <musickappa1802@gmail.com>
2020-11-12 19:10:38 +0900

    inital

---
+++ test.php
@@ -0,0 +1,12 @@
+<!-- MYSQLからのデータの取得 -->
+<!-- https://qiita.com/JUM22676603/items/8d6376bcfaa9abe7f55f -->
+<!-- MYSQLへのデータの挿入 -->
+<!-- https://taupe.site/entry/line-messaging-api/ -->
+<!-- MYSQLのコマンド一覧 -->
+<!-- https://qiita.com/maruyama42/items/a692edac9f5aa4b64786 -->
+
+<?php
+
+echo 'Hello World!';
+
+?>
